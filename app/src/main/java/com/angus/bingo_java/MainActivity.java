65f04bbee7d76be726ba4c56f320ccb167d92de9
pighaddt <pighaddt@gmail.com>
2020-05-12 16:55:50 +0800

    讀取Firebase儲存資料庫

--- app/src/main/java/com/angus/bingo_java/MainActivity.java
+++ app/src/main/java/com/angus/bingo_java/MainActivity.java
@@ -3,39 +3,48 @@ package com.angus.bingo_java;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
+import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int RC_SIGN_IN = 100;
+    private FirebaseAuth auth;
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseAuth.getInstance().addAuthStateListener(this);
+        auth.addAuthStateListener(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        FirebaseAuth.getInstance().removeAuthStateListener(this);
+        auth.removeAuthStateListener(this);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -71,11 +80,48 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
             Log.d(TAG, "onAuthStateChanged:" +
                     auth.getCurrentUser().getEmail() + "/" +
                     auth.getCurrentUser().getUid());
-            String displayName = user.getDisplayName();
+            final String displayName = user.getDisplayName();
             FirebaseDatabase.getInstance().getReference("users")
                     .child(user.getUid())
                     .child("displayName")
                     .setValue(displayName);
+            FirebaseDatabase.getInstance().getReference("users")
+                    .child(user.getUid())
+                    .child("nickName")
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.getValue() != null){
+                                String nickname = (String) dataSnapshot.getValue();
+                            }else{
+                                showNickNameDialog(displayName);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
         }
+    }
+
+    private void showNickNameDialog(String displayName) {
+        final EditText nickEdit = new EditText(this);
+        nickEdit.setText(displayName);
+        new AlertDialog.Builder(this)
+                .setTitle("Your Nickname")
+                .setMessage("Please enter your nickname")
+                .setView(nickEdit)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nickname = String.valueOf(nickEdit.getText());
+                        FirebaseDatabase.getInstance().getReference("users")
+                                .child(auth.getUid())
+                                .child("nickName")
+                                .setValue(nickname);
+                    }
+                }).show();
     }
 }
