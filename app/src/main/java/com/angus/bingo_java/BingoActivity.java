2dd5a1289303c247a1a2168592f0dddecdf06955
pighaddt <pighaddt@gmail.com>
2020-05-27 17:05:01 +0800

    25 顆球的格狀清單設計，debug除錯實務

--- app/src/main/java/com/angus/bingo_java/BingoActivity.java
+++ app/src/main/java/com/angus/bingo_java/BingoActivity.java
@@ -1,14 +1,21 @@
 package com.angus.bingo_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,6 +26,7 @@ public class BingoActivity extends AppCompatActivity {
     private static final String TAG = BingoActivity.class.getSimpleName();
     private TextView infomation;
     private RecyclerView recycler;
+    private FirebaseRecyclerAdapter<Boolean, NumberHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +44,57 @@ public class BingoActivity extends AppCompatActivity {
                         .setValue(false);
             }
         }
-        List<NumberButton> buttons = new ArrayList<>();
+        final List<NumberButton> buttons = new ArrayList<>();
         for (int i = 0; i < 25; i++) {
             NumberButton button = new NumberButton(BingoActivity.this);
             button.setNumber(i+1);
+            buttons.add(button);
         }
         Collections.shuffle(buttons);
+        //RecyclerView
+
+        Query query = FirebaseDatabase.getInstance().getReference("rooms")
+                .child(roomId)
+                .child("numbers")
+                .orderByKey();
+        FirebaseRecyclerOptions<Boolean> options = new FirebaseRecyclerOptions.Builder<Boolean>()
+                .setQuery(query, Boolean.class)
+                .build();
+        adapter = new FirebaseRecyclerAdapter<Boolean, NumberHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull NumberHolder holder, int position, @NonNull Boolean model) {
+                holder.button.setText(String.valueOf(buttons.get(position).getNumber()));
+                holder.button.setEnabled(!model);
+            }
+
+            @NonNull
+            @Override
+            public NumberHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(BingoActivity.this)
+                        .inflate(R.layout.single_button, parent, false);
+                return new NumberHolder(view);
+            }
+        };
         findViews();
+    }
+    class NumberHolder extends RecyclerView.ViewHolder{
+        NumberButton button;
+        public NumberHolder(@NonNull View itemView) {
+            super(itemView);
+             button = itemView.findViewById(R.id.button);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     private void findViews() {
@@ -50,5 +102,6 @@ public class BingoActivity extends AppCompatActivity {
         recycler = findViewById(R.id.recycler);
         recycler.setHasFixedSize(true);
         recycler.setLayoutManager(new GridLayoutManager(BingoActivity.this, 5));
+        recycler.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/angus/bingo_java/NumberButton.java
+++ app/src/main/java/com/angus/bingo_java/NumberButton.java
@@ -6,23 +6,25 @@ import android.widget.Button;
 
 public class NumberButton extends androidx.appcompat.widget.AppCompatButton {
     int number;
-    Boolean enabled;
+    Boolean picked;
+    int position;
 
     public int getNumber() {
         return number;
+    }
+
+    public Boolean getPicked() {
+        return picked;
+    }
+
+    public void setPicked(Boolean picked) {
+        this.picked = picked;
     }
 
     public void setNumber(int number) {
         this.number = number;
     }
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
 
     public int getPosition() {
         return position;
@@ -32,7 +34,6 @@ public class NumberButton extends androidx.appcompat.widget.AppCompatButton {
         this.position = position;
     }
 
-    int position;
     public NumberButton(Context context) {
         super(context);
     }
