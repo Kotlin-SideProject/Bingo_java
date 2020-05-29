706015d01fda0edcde1f0a906e22a3d710d3813f
pighaddt <pighaddt@gmail.com>
2020-05-29 12:45:21 +0800

    設計能夠對應號碼球的位置，更動資料時的處理

--- app/src/main/java/com/angus/bingo_java/BingoActivity.java
+++ app/src/main/java/com/angus/bingo_java/BingoActivity.java
@@ -12,14 +12,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.common.ChangeEventType;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BingoActivity extends AppCompatActivity {
 
@@ -44,6 +48,8 @@ public class BingoActivity extends AppCompatActivity {
                         .setValue(false);
             }
         }
+        // map for number to position
+        final Map<Integer, Integer> numberMap =new HashMap();
         final List<NumberButton> buttons = new ArrayList<>();
         for (int i = 0; i < 25; i++) {
             NumberButton button = new NumberButton(BingoActivity.this);
@@ -51,6 +57,9 @@ public class BingoActivity extends AppCompatActivity {
             buttons.add(button);
         }
         Collections.shuffle(buttons);
+        for (int i = 0; i < 25; i++) {
+            numberMap.put(buttons.get(i).getNumber(), i);
+        }
         //RecyclerView
 
         Query query = FirebaseDatabase.getInstance().getReference("rooms")
@@ -64,7 +73,20 @@ public class BingoActivity extends AppCompatActivity {
             @Override
             protected void onBindViewHolder(@NonNull NumberHolder holder, int position, @NonNull Boolean model) {
                 holder.button.setText(String.valueOf(buttons.get(position).getNumber()));
-                holder.button.setEnabled(!model);
+//                holder.button.setEnabled(!model);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull ChangeEventType type, @NonNull DataSnapshot snapshot, int newIndex, int oldIndex) {
+                super.onChildChanged(type, snapshot, newIndex, oldIndex);
+                Log.d(TAG, "onChildChanged: " + type + "/" +snapshot.getKey() + "/" + snapshot.getValue());
+                if (type == ChangeEventType.CHANGED){
+                    int numbert = Integer.parseInt(snapshot.getKey());
+                    Boolean picked = (Boolean) snapshot.getValue();
+                    int position = numberMap.get(numbert);
+                    NumberHolder holder = (NumberHolder) recycler.findViewHolderForAdapterPosition(position);
+                    holder.button.setEnabled(!picked);
+                }
             }
 
             @NonNull
